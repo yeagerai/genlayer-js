f75fa6a358a8bdba445d00185fe40c54a7a96fdc
Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>
2024-11-07 08:52:56 -0300

    fix tests
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from "vitest/config";
+import {defineConfig} from "vitest/config";
+import path from "path";
 
 export default defineConfig({
   test: {
@@ -7,6 +8,11 @@ export default defineConfig({
     coverage: {
       reporter: ["text", "json", "html"],
       exclude: ["node_modules/"],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
