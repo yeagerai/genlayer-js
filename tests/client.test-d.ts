7dc726fa4fc8769feaec07a50149956d7c2a2035
Agustín Díaz <49416765+AgustinRamiroDiaz@users.noreply.github.com>
2024-11-14 11:50:13 -0300

    feat: add contract schema methods (#26)
    Signed-off-by: Agustín Ramiro Díaz <agustin.ramiro.diaz@gmail.com>

--- tests/client.test-d.ts
+++ tests/client.test-d.ts
@@ -8,26 +8,32 @@ test("type checks", () => {
     account: createAccount(generatePrivateKey()),
   });
 
+  const exampleAddress = "0x1234567890123456789012345678901234567890";
+
   // This should fail type checking - "whatever" is not a valid filter
   // @ts-expect-error "whatever" is not a valid filter type
   void client.request({
     method: "sim_getTransactionsForAddress",
-    params: ["0x1234567890123456789012345678901234567890", "whatever"],
+    params: [exampleAddress, "whatever"],
   });
 
   // This should pass type checking - "all", "to" and "from" are valid filters
   void client.request({
     method: "sim_getTransactionsForAddress",
-    params: ["0x1234567890123456789012345678901234567890", "all"],
+    params: [exampleAddress, "all"],
   });
 
   void client.request({
     method: "sim_getTransactionsForAddress",
-    params: ["0x1234567890123456789012345678901234567890", "to"],
+    params: [exampleAddress, "to"],
   });
 
   void client.request({
     method: "sim_getTransactionsForAddress",
-    params: ["0x1234567890123456789012345678901234567890", "from"],
+    params: [exampleAddress, "from"],
   });
+
+  void client.getContractSchema(exampleAddress);
+
+  void client.getContractSchemaForCode("class SomeContract...");
 });
