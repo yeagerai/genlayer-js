b923e7e313fbd7edc10f04994796440075658b03
Edinaldo Junior <epsjunior@icloud.com>
2024-12-20 05:58:59 -0300

    fix: always using metamask

--- src/client/client.ts
+++ src/client/client.ts
@@ -21,12 +21,13 @@ interface ClientConfig {
 export const createClient = (config: ClientConfig = { chain: simulator }) => {
   const chainConfig = config.chain || simulator;
   const rpcUrl = config.endpoint || chainConfig.rpcUrls.default.http[0];
+  const isAddress = typeof config.accountOrAddress !== "object";
 
   const customTransport = {
       async request({method, params}: {method: string; params: any[]}) {
-        if (method.startsWith('eth_') && window.ethereum) {
+        if (method.startsWith('eth_') && isAddress) {
           try {
-            return await window.ethereum.request({method, params});
+            return await window.ethereum?.request({method, params});
           } catch (err) {
             console.warn(`Error using window.ethereum for method ${method}:`, err);
             throw err;
