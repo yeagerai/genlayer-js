70189fda34bfb833311dc788c2d746f8c6fdc0b8
Cristiam Da Silva <cristiam86@gmail.com>
2025-05-08 02:11:15 +0200

    refactor: defined GenLayer Transaction type with Votes and Results

--- src/types/transactions.ts
+++ src/types/transactions.ts
@@ -1,3 +1,4 @@
+import {Hex} from "viem";
 import {Address} from "./accounts";
 
 export type Hash = `0x${string}` & {length: 66};
@@ -18,6 +19,22 @@ export enum TransactionStatus {
   READY_TO_FINALIZE = "READY_TO_FINALIZE",
   VALIDATORS_TIMEOUT = "VALIDATORS_TIMEOUT",
   LEADER_TIMEOUT = "LEADER_TIMEOUT",
+}
+
+export enum TransactionResult {
+  SUCCESS = "SUCCESS",
+  FAILURE = "FAILURE",
+}
+
+export enum TransactionResult {
+  IDLE = "IDLE",
+  AGREE = "AGREE",
+  DISAGREE = "DISAGREE",
+  TIMEOUT = "TIMEOUT",
+  DETERMINISTIC_VIOLATION = "DETERMINISTIC_VIOLATION",
+  NO_MAJORITY = "NO_MAJORITY",
+  MAJORITY_AGREE = "MAJORITY_AGREE",
+  MAJORITY_DISAGREE = "MAJORITY_DISAGREE",
 }
 
 export const transactionsStatusNumberToName = {
@@ -54,12 +71,156 @@ export const transactionsStatusNameToNumber = {
   [TransactionStatus.LEADER_TIMEOUT]: "13",
 };
 
+export const transactionResultNumberToName = {
+  "0": TransactionResult.IDLE,
+  "1": TransactionResult.AGREE,
+  "2": TransactionResult.DISAGREE,
+  "3": TransactionResult.TIMEOUT,
+  "4": TransactionResult.DETERMINISTIC_VIOLATION,
+  "5": TransactionResult.NO_MAJORITY,
+  "6": TransactionResult.MAJORITY_AGREE,
+  "7": TransactionResult.MAJORITY_DISAGREE,
+};
+
+export const TransactionResultNameToNumber = {
+  [TransactionResult.IDLE]: "0",
+  [TransactionResult.AGREE]: "1",
+  [TransactionResult.DISAGREE]: "2",
+  [TransactionResult.TIMEOUT]: "3",
+  [TransactionResult.DETERMINISTIC_VIOLATION]: "4",
+  [TransactionResult.NO_MAJORITY]: "5",
+  [TransactionResult.MAJORITY_AGREE]: "6",
+  [TransactionResult.MAJORITY_DISAGREE]: "7",
+};
+
+export enum VoteType {
+  NOT_VOTED = "NOT_VOTED",
+  AGREE = "AGREE",
+  DISAGREE = "DISAGREE",
+  TIMEOUT = "TIMEOUT",
+  DETERMINISTIC_VIOLATION = "DETERMINISTIC_VIOLATION",
+}
+
+export const voteTypeNumberToName = {
+  "0": VoteType.NOT_VOTED,
+  "1": VoteType.AGREE,
+  "2": VoteType.DISAGREE,
+  "3": VoteType.TIMEOUT,
+  "4": VoteType.DETERMINISTIC_VIOLATION,
+};
+
+export const voteTypeNameToNumber = {
+  [VoteType.NOT_VOTED]: "0",
+  [VoteType.AGREE]: "1",
+  [VoteType.DISAGREE]: "2",
+  [VoteType.TIMEOUT]: "3",
+  [VoteType.DETERMINISTIC_VIOLATION]: "4",
+};
+
+export type TransactionType = "deploy" | "call";
+
+export type DecodedDeployData = {
+  code?: Hex;
+  constructorArgs?: any; // Type this more strictly if possible
+  leaderOnly?: boolean;
+  type?: TransactionType;
+  contractAddress?: Address;
+};
+
+export type DecodedCallData = {
+  callData?: any; // Type this more strictly if possible
+  leaderOnly?: boolean;
+  type: TransactionType;
+};
+
+// TODO: make localnet compatible with testnet and unify the types
 export type GenLayerTransaction = {
-  hash: TransactionHash;
-  status: TransactionStatus;
-  from_address?: string;
-  to_address?: string;
+  // currentTimestamp: testnet
+  currentTimestamp?: string;
+
+  // from_address: localnet // sender: testnet
+  from_address?: Address;
+  sender?: Address;
+
+  // to_address: localnet // recipient: testnet
+  to_address?: Address;
+  recipient?: Address;
+
+  // numOfInitialValidators: testnet
+  numOfInitialValidators?: string;
+
+  // txSlot: testnet
+  txSlot?: string;
+
+  // createdTimestamp: testnet
+  createdTimestamp?: string;
+
+  // lastVoteTimestamp: testnet
+  lastVoteTimestamp?: string;
+
+  // randomSeed: testnet
+  randomSeed?: Hash;
+
+  // result: testnet
+  result?: number;
+  resultName?: TransactionResult;
+
+  // data: localnet // txData: testnet
   data?: Record<string, unknown>;
+  txData?: Hex;
+  txDataDecoded?: DecodedDeployData | DecodedCallData;
+  // txReceipt: testnet
+  txReceipt?: Hash;
+
+  // messages: testnet
+  messages?: unknown[];
+
+  // queueType: testnet
+  queueType?: number;
+
+  // queuePosition: testnet
+  queuePosition?: string;
+
+  // activator: testnet
+  activator?: Address;
+
+  // lastLeader: testnet
+  lastLeader?: Address;
+
+  // status: localnet: TransactionStatus // status: testnet: number
+  status?: TransactionStatus | number;
+  statusName?: TransactionStatus;
+
+  // hash: localnet // txId: testnet// hash: localnet // txId: testnet
+  hash?: TransactionHash;
+  txId?: TransactionHash;
+
+  // readStateBlockRange: testnet
+  readStateBlockRange?: {
+    activationBlock: string;
+    processingBlock: string;
+    proposalBlock: string;
+  };
+
+  // numOfRounds: testnet
+  numOfRounds?: string;
+
+  // lastRound: testnet
+  lastRound?: {
+    round: string;
+    leaderIndex: string;
+    votesCommitted: string;
+    votesRevealed: string;
+    appealBond: string;
+    rotationsLeft: string;
+    result: number;
+    roundValidators: Address[];
+    validatorVotesHash: Hash[];
+    validatorVotes: number[];
+    validatorVotesName: VoteType[];
+  };
+
+  // consensus_data: localnet // leader_receipt: testnet
   consensus_data?: {
     final: boolean;
     leader_receipt?: {
@@ -99,7 +260,7 @@ export type GenLayerRawTransaction = {
   lastVoteTimestamp: bigint;
   randomSeed: Hash;
   result: number;
-  txData: string;
+  txData: Hex | undefined | null;
   txReceipt: Hash;
   messages: unknown[];
   queueType: number;
